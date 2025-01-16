77f223d131ffd6783b8af694a81a92c405cbf8d7
Devin Ma <dvma7807@gmail.com>
2025-01-16 04:59:53 +0000

    cleaned up code

--- src/reassembler.hh
+++ src/reassembler.hh
@@ -44,13 +44,10 @@ public:
 
 private:
   ByteStream output_;
-  // std::map<uint64_t, std::string> cache_ {};
-  // std::vector<char> cache_{};
   std::map<uint64_t, std::pair<uint64_t, std::string>> cache_{};
   bool cache_initialized = false;
   bool finish = false;
   uint64_t finish_index = -1;
-  // uint64_t cache_size = 0;
   uint64_t bytes_pending = 0;
   int first_known = -1;
 };
